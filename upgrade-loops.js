e9c0085010f7ecf159ace8635e32d52d6eeae4a0
ricardograna <ricardogranarosales@gmail.com>
2022-10-11 10:41:20 +0200

    feat. iteración5

--- upgrade-loops.js
+++ upgrade-loops.js
@@ -95,7 +95,22 @@ for(const info in alien){
 
 //Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un ***console log*** el array. Puedes usar este array:
 
-const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+const placesToTravel = [
+    {id: 5, name: 'Japan'}, 
+    {id: 11, name: 'Venecia'}, 
+    {id: 23, name: 'Murcia'}, 
+    {id: 40, name: 'Santander'}, 
+    {id: 44, name: 'Filipinas'}, 
+    {id: 59, name: 'Madagascar'}
+];
+
+for(let i = 0; i < placesToTravel.length; i++){
+    let placeToTravel = placesToTravel[i];
+
+    if(placeToTravel.id === 11 || placeToTravel.id === 40) placesToTravel.splice(i,1);
+}
+
+console.log(placesToTravel);
 
 
 //**Iteración #6: Mixed For e includes**
